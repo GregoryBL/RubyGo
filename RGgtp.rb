7f0d7697b5c31a0dc56e296d51218656c0b8bd7a
Greg Berns-Leone <gberns.l@gmail.com>
2013-04-20 08:04:51 -0500

    Beginnings of a gtp module

---
+++ RGgtp.rb
@@ -0,0 +1,18 @@
+class RGgtp
+
+    def initialize
+        cols = ["A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T"]
+        ARGV.each do|a|
+            
+        end
+    end
+    
+    def get_input
+        
+    end
+    
+    def output
+        
+    end
+    
+end
